a4ea3fb8623ec663a553a92ce6cdc02b23ac45c6
ArsenUa <mr.landos@gmail.com>
2018-10-12 22:08:26 -0700

    Update simulatorTest.py

--- simulatorTest.py
+++ simulatorTest.py
@@ -8,23 +8,19 @@ import page
 
 # Test cases   
 class Tests(unittest.TestCase): 
-
-    @classmethod
-    def setUpClass(self):
+    
+    def setUp(self):
         self.desired_caps = desired_capabilities.get_desired_capabilities('TestApp.app.zip')
         self.driver = webdriver.Remote('http://localhost:4723/wd/hub', self.desired_caps) 
-
-    def setUp(self):
-        first_field = page.input_first_field(self,3)
-        second_field = page.input_second_field(self,2)
-        comp = page.copute_sum(self)
-
-    @classmethod
+      
     def tearDownClass(self):
         cmd = 'killall Simulator'
         os.system(cmd)
  
-    def test_compute_sum(self):                   
+    def test_compute_sum(self):  
+        first_field = page.input_first_field(self,3)
+        second_field = page.input_second_field(self,2)
+        comp = page.copute_sum(self)
         self.assertEqual(page.get_compute_sum(self), "5")  
 
 if __name__ == '__main__':
